558a3d641cd614a76231066ae0e09459042d6e37
Julien Vehent <julien@linuxwall.info>
2016-07-23 19:31:26 -0400

    update deployment scripts

--- create_ebs_env.sh
+++ create_ebs_env.sh
@@ -34,8 +34,6 @@ dockerstack="$(aws elasticbeanstalk list-available-solution-stacks | \
     jq -r '.SolutionStacks[]' | grep -P '.+Amazon Linux.+Docker.+' | head -1)"
 
 # Create the EB API environment
-sed "s/POSTGRESPASSREPLACEME/$dbpass/" ebs-options.json > tmp/$identifier/ebs-options.json || fail
-sed -i "s/POSTGRESHOSTREPLACEME/$dbhost/" tmp/$identifier/ebs-options.json || fail
 aws elasticbeanstalk create-environment \
     --application-name $identifier \
     --environment-name deployer-api \
@@ -48,11 +46,11 @@ echo "API environment $apieid is being created"
 
 # Upload the application version
 aws s3 mb s3://$identifier
-aws s3 cp ebs.json s3://$identifier/
+aws s3 cp app-version.json s3://$identifier/
 aws elasticbeanstalk create-application-version \
     --application-name "$identifier" \
     --version-label deployer-api \
-    --source-bundle "S3Bucket=$identifier,S3Key=ebs.json" > tmp/$identifier/appversion.json
+    --source-bundle "S3Bucket=$identifier,S3Key=app-version.json" > tmp/$identifier/appversion.json
 
 # Wait for the environment to be ready (green)
 echo -n "waiting for environment"
